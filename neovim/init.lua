d0297c223807b6230b75cc9bcbe0d449a50e246c
allen <chunkystyles@gmail.com>
2025-11-13 20:18:32 -0500

    Added coc-java

--- neovim/init.lua
+++ neovim/init.lua
@@ -4,7 +4,7 @@ vim.g.maplocalleader = " "
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
-require("config.commands")
+require("config.autocommands")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
